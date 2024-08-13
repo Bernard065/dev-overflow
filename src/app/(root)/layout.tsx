6b403b928a6981649997a694a7e567b85d88ef7f
Bernard065 <bebenibernard@gmail.com>
2024-08-14 02:30:55 +0300

    Implementing the leftsidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSideBar from "@/components/shared/LeftSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -10,7 +11,8 @@ export default function RootLayout({
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        LeftSidebar
+        <LeftSideBar />
+        
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
