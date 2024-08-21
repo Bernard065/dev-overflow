f3e2dd889752ccd9da73696a832647a479c79632
Bernard065 <bebenibernard@gmail.com>
2024-08-21 19:56:53 +0300

    updating the WEBHOOK_SECRET

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -2,16 +2,12 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import {
-  createUser,
-  deleteUser,
-  updateUser,
-} from "@/lib/actions/user.action";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
